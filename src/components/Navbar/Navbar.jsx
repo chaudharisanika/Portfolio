0c90bfd0c9a31b1f65fbd74153e28261e1503a7f
Sanika Chaudhari <sanikachaudhari201003@gmail.com>
2024-01-26 18:36:53 +0530

    done

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+<>
+ <nav className='nav-wrapper'>
+    <div className='nav-content'>
+        <img className='logo' src='../assets/images/logo.jpg' alt=''/>
+        <ul>
+            <li>
+                <a className='menu-item'>Home</a>
+            </li>
+            <li>
+                <a className='menu-item'>Skills</a>
+            </li>
+            <li>
+                <a className='menu-item'>Projects</a>
+            </li>
+            <li>
+                <a className='menu-item'>Contact Me</a>
+            </li>
+            <button className='contact-btn' onClick={() => {}}>Hire Me</button>
+        </ul>
+    </div>
+ </nav>
+
+</>  
+)
+}
+
+export default Navbar
